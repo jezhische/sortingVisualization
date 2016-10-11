2dc9f63f49b436114fe75437ca62d7254683bcee
jezhische <jezhische@gmail.com>
2016-10-11 19:00:10 +0300

    adding new files

---
+++ src/main/java/frame/BasicFrame.java
@@ -0,0 +1,18 @@
+package frame;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by WORK on 11.10.2016.
+ */
+public class BasicFrame extends JFrame implements ActionListener {
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
